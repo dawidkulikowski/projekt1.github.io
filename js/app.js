924f3fa39d4e6d936a56fa7a42ac31867b7c0d4c
dawidkulikowski <dawidkulikowski1994@gmail.com>
2017-10-24 21:07:13 +0200

    pierwszy wrzut

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$("#arrow").on("click", function () {
+
+	$("body, html").animate( 
+	{
+		scrollTop:$("#about").offset().top-50},1200)
+
+
+
+})
+
+// document.addEventListener('DOMContentLoaded')
+
+// var arrow = document.querySelector('#arrow');
+
+// arrow.addEventListener('click', function() {
+
+// 	alert('nacisniety');
+// })
